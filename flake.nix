5bd7337d4a967eadb1cc5cb244ab64c1cf73e169
Mattia Nicolella <mattianicolella@gmail.com>
2022-02-01 20:50:31 -0500

    feat: add nix flake and direnv config
    The developnment enviroment is now reprodiceble thanks to a nix flake and direnv (which will
    atcivate the flake upon directory entry)

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A reproducible environment for rt-bench";
+  inputs ={
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  }; 
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
+    {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            (pkgs.python3.withPackages (p: [p.numpy p.matplotlib]))
+            pkgs.gcc
+            pkgs.util-linux
+            pkgs.gnugrep
+            pkgs.coreutils
+            pkgs.ps
+          ];
+        };
+    }
+    );
+}
