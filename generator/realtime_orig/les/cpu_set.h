066a987ab2ada2de576527837c2f6eada5c7b4c0
Zhenting Huang <3061613175@qq.com>
2026-02-23 20:42:30 +0800

    fix(realtime): Fix cpu_set.h for fresh RT-Thread clone

--- generator/realtime_orig/les/cpu_set.h
+++ generator/realtime_orig/les/cpu_set.h
@@ -1,36 +1,28 @@
 /* cpu_set.h
- * 
+ * CPU affinity support for RT-Thread
  */
 
 #ifndef __CPU_SET_H__
 #define __CPU_SET_H__
 
-#include <pthread.h>
+#include <stddef.h>
 
+/* 使用核心数 */
+#define USE_PROCESSORS 2
 
-
-/* 使用核心数。注意：请勿修改此数。此数设置为2，表示多核测试部分均只使用两个核心进行测试，*/
-#define USE_PROCESSORS 2  
-
-
-// 示例实现
-#ifdef __RT_THREAD_H__
-
+/* cpu_set_t definition */
+#ifndef cpu_set_t
 typedef unsigned long cpu_set_t;
-
-#define CPU_ZERO(cpusetp)		(*(cpusetp) = 0)
-#define CPU_SET(cpu, cpusetp)	(*(cpusetp) |= (1UL << (cpu)))
-#define CPU_ISSET(cpu, cpusetp)	(*(cpusetp) & (1UL << (cpu)))
-
 #endif
-//#else ...
 
+#define CPU_ZERO(cpusetp)       (*(cpusetp) = 0)
+#define CPU_SET(cpu, cpusetp)   (*(cpusetp) |= (1UL << (cpu)))
+#define CPU_ISSET(cpu, cpusetp) (*(cpusetp) & (1UL << (cpu)))
 
-
-
-
-
-
+/* pthread_setaffinity_np - only when pthread is available */
+#ifdef __RT_THREAD_H__
+#include <pthread.h>
 int pthread_setaffinity_np(pthread_t thread, size_t cpusetsize, const cpu_set_t *cpuset);
+#endif
 
-#endif /* __CPU_SET.H__ */
+#endif /* __CPU_SET_H__ */
